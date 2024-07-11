2ccee48ae0836a4338f4423c1b11075a14dbdbfe
mattmaximo <mattmaximo@hotmail.com>
2024-07-11 10:20:30 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="coingecko_exporter",
     version="0.1.0",
