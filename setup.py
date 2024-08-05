7e06267d9152b381faef8067c91194c1ef6a4b0d
mattmaximo <mattmaximo@hotmail.com>
2024-08-05 15:07:33 -0400

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="coingecko_exporter",  
-    version="0.1.8",
+    version="0.1.9",
     description="A package to export bulk data from the CoinGecko API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,7 +23,7 @@ setup(
         "pyarrow",
         "fastparquet",
         "boto3",
-        
+
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
